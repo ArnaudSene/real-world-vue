5237aca5e13db32a17a2ed32e7f7fb6cc07c1f37
Arnaud SENE <arnaud.sene@pm.me>
2022-05-06 16:42:58 -0400

    L4-end up and running app - routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home,
+    path: '/', // url
+    name: 'EventList', // Name of the route
+    component: EventList, // which 'view' component to render
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/about',
+    name: 'about',
+    component: About,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
